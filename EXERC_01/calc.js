6ed36163de3c723c8f6cd924745a36b66ed847f2
Paulo César <paulo_cesarcanal@hotmail.com>
2023-01-01 13:08:57 -0300

    CALCULADORA DE MÉDIA
    Bom consegui, era ate mais simples que eu pensei

--- EXERC_01/calc.js
+++ EXERC_01/calc.js
@@ -1,11 +1,13 @@
-var n1 = 4;
-var n2 = 4;
-var n3 = 6;
-var n4 = 8;
+var n1 = document.querySelector('#nota1');
+var n2 = document.querySelector('#nota2');
+var n3 = document.querySelector('#nota3');
+var n4 = document.querySelector('#nota4');
+var vmedia = 4;
+var media = document.querySelector('span');
 
-var soma = n1 + n2 + n3 + n4;
-
-var media = soma/4;
+function soma() {
+    media.innerHTML = (parseInt(n1.value) + parseInt(n2.value) + parseInt(n3.value) + parseInt(n4.value)) / (parseInt(vmedia));
+}
 
 
 
