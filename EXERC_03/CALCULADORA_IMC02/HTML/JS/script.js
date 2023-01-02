27d2f0c4f2d9c1446fea98627e18bda5ab2422ae
Paulo César <paulo_cesarcanal@hotmail.com>
2023-01-02 18:21:52 -0300

    CALCULADORA DE IMC 02
    Copiei e colei, um trecho de código, mas acho que vai da problema!!

---
+++ EXERC_03/CALCULADORA_IMC02/HTML/JS/script.js
@@ -0,0 +1,38 @@
+// DADOS IMC
+dados  const =  [
+    {
+      min:0,
+      máx:18.4,
+      classificação:"Menor que 18,5",
+      info:"Magreza",
+      obesidade:"0",
+    } ,
+    {
+      min:18.5,
+      máx:24.9,
+      classificação: "Entre 18,5 e 24,9",
+      info: "Normal",
+      obesidade: "0",
+    } ,
+    {
+      min: 25,
+      máx:29.9,
+      classificação:"Entre 25,0 e 29,9",
+      info:"Sobrepeso",
+      obesidade:"I",
+    } ,
+    {
+      min: 30,
+      máx: 39.9,
+      classificação:"Entre 30,0 e 39,9",
+      info:"Obesidade",
+      obesidade:"II",
+    } ,
+    {
+      min:40,
+      máx:99,
+      classificação:"Maior que 40,0",
+      info:"Obesidade grave",
+      obesidade:"III",
+    } ,
+  ] ;
